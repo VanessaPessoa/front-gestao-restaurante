4396ab3a8ff5f2bb73cfe491dd20ffab350c3245
cmal-u <43617346+cmal-u@users.noreply.github.com>
2023-05-26 21:07:13 -0300

    Update index.js

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -8,7 +8,36 @@ export default function Admin(){
     console.log()
     return (
        <Container>
-                      
+           <Title>
+                Funcionários
+           </Title>
+
+            <IconButton color="primary" aria-label="adicionar novo funcionario no sistema">
+                <Add />
+            </IconButton>
+           <Box>
+                {employee ? 
+                 employee.map(item => (
+                    <div key={item.id}>
+                       <div>
+                        <p>
+                                Nome: {item.name}
+                        </p>
+                        <p>
+                                E-mail: {item.email}
+                        </p>
+                       </div>
+                       <IconButton color="primary" aria-label="editar informação usuário">
+                        <Edit />
+                       </IconButton>
+                    </div>
+                 ))
+                : 
+                    <p> Não possui funcionários cadastrados</p>
+                }
+           </Box>
+
+            
        </Container>
     )
 }
