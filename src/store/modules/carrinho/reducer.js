6c9d607094fe1569ef6743c88508fa30ee9841a5
cmal-u <maria.calixto@rpe.tech>
2023-06-16 11:01:11 -0300

    Adicionando a action de adicionar o item no carrinho

--- src/store/modules/carrinho/reducer.js
+++ src/store/modules/carrinho/reducer.js
@@ -5,8 +5,13 @@ const initialState = [];
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.ADD_ITEM:
-      // Substituir o codigo deste item
-     return;
+      const {id, name, price} = action.payload;
+      const itemExists = state.find(item => item.id === id);
+      if (itemExists) {
+        return state.map(item => item.id === id ? {...item, quantidade: item.quantidade + 1} : item);
+      } else {
+        return [...state, {id, name, price, quantidade: 1}];
+      }
     case types.REMOVE_ITEM:
       const itemId = action.payload;
       return state.filter(item => item.id !== itemId);
